6b5968c690c618caf3e073d5f483f9e720cf5b17
Marie Gosme <marie.gosme@inra.fr>
2023-07-28 14:13:52 +0200

    added contributors

--- ui.R
+++ ui.R
@@ -18,8 +18,9 @@ sidebar <- dashboardSidebar(
     inline = TRUE),
   sidebarMenu(
     
-    menuItem("Informations", tabName = "infos"),
-    menuItem("Tool", tabName = "tool")
+    menuItem("Informations", tabName = "Welcome"),
+    menuItem("Tool", tabName = "tool"),
+    menuItem("Databases", tabName = "databases")
   )
 )
 
@@ -28,9 +29,17 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   
   tabItems(
-    tabItem(tabName = "infos", 
-            h1("Welcome! please go to Tool in the menu"),
-            img(src = "logoDigitAF_HR_500.png")
+    tabItem(tabName = "Welcome", 
+            h1("Welcome! please go to Tool in the menu to find the best tree for you! or to the Databases to learn where all this comes from"),
+            h1("This tool is under development (to do: make this welcome page look nicer!) within the DigitAF European project"),
+            img(src = "logoDigitAF_HR_500.png"),
+            h1("Contributors:"),
+            h2("Contributors to code"),
+            h2("Marie Gosme; Clément Rigal; Raphael Paut"),
+            h2("Contributors to databases"),
+            h2("DENTRO: Sarah Carton; Paul Pardon; Bert Reubens"),
+            h2("Deciduous: Raphael Paut; François Warlop"),
+            h2("Shade Tree Advice: Clément Rigal; Philippe Vaast; Laurence Jassogne; Just van der Wolf; Gilles Gram; Mathilde LEPINE; Anais CARPENTE; Mai Phuong NGUYEN; Sigrun WAGNER; Sophie GRAEFE; Baptiste CAMUS; Juan Carlos Villalba Malaver")
     ),#fin infos
     tabItem(tabName = "tool", 
             tabsetPanel(
@@ -42,9 +51,15 @@ body <- dashboardBody(
               tabPanel("Deciduous (fruit trees in France)", moduleTabInterface_UI(id = "DECIDUOUS", data=dataDECIDUOUS, interface=interfaceDECIDUOUS)) # module selecmap avec les données de l'Asie
               
             )
-            
+    ),#fin tool
+    tabItem(tabName = "databases", 
+            h1("Coming soon! Inventory of all (known to us) tools for tree selection in agroforestry, with:"),
+            h2("link to the original tool"),
+            h2("filtering by usefull features"),
+            h2("symbol indicating if the tool is integrated in the unified tool")
+    
             # A completer: page tutoriel video, page pdf backend
-    )#fin tool
+    )#fin databases
   ) #fin pages
 ) #fin dashboardbody
 
