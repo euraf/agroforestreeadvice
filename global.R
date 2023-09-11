3c342a0c1b2709232e38eb24375c33dd0bc8a92d
Marie Gosme <marie.gosme@inra.fr>
2023-09-11 17:19:16 +0200

    computs effectiveness SCSM (not finished)

--- global.R
+++ global.R
@@ -480,6 +480,17 @@ compute_suitability_DECIDUOUS<-function(inputsdata=NULL,
   
 }
 
+#' compute_suitability for SCSM data
+#'
+#' @param inputsdata named character vector of choices (for response traits) or Big criteria (for effect traits) made by the user, warning: these are the values internal to the interface, not the labels seen on the screen (which depend on language)
+#' @param database data.frame of tree characteristics, with columns  , 
+#' @param interface data.frame describing the interface of the app for this database, with columns "initialorder" (not used), "side" (either reponsetrait or effecttrait), "order" (not used), "BigCriteria", "criteria", "choice", "objecttype" (checkbox, Selectinput etc...), "weightwithincriteria" (not used for now), "BigCriteria_en", "criteria_en", "choice_en" and other colums for translations in other languages
+#' @param orderby either effecttrait or reponsetrait, for final ordering of the data.frame rows
+#'
+#' @return A data.frame with names "side", "BigCriteria", "English.name", "Scientific.name", "value", where side, big criteria (but only those relevant to inputdata choices, except for the effecttraits side (if no big criteria chosen, then keep all))"English.name" and "Scientific.name" are the same as in database, and value is computed to be the score for each tree for each retained big criteria, and species is an ordered factor (ordered by sum of effecttraits or responsetraits depending on argument orderby)
+#' @export
+#'
+#' @examples compute_suitability(inputsdata = c(countryregion="Vietnam (North-West Vietnam)", crop="Arabica coffee",	precipitation="Medium precipitation","biodiversity"), database=database, interface=interface)
 
 compute_suitability_SCSM<-function(inputsdata=NULL,
                                         database, 
@@ -514,7 +525,26 @@ compute_suitability_SCSM<-function(inputsdata=NULL,
   
   dbfinal<-rbind(database_temp, database_precipitation)
   
-  #to allow correct display in the barplot, values for response traits
+  #todo, compute effectiveness based on effect traits and inputdata of objectives
+  database['BigCriteria']='climate'
+  database['side']='responsetrait'
+  computecrit<-function(characteristics,type=c("checkboxgroup", "selectinrange"),inputs){
+    if (type=="checkboxgroup"){
+      #strsplit the inputs to see which were chosen, strsplit the characteristics to see all that is provided by the species, 
+      #count the number of characteristics %in% inputs to get the score
+      #then divide by the number of possibilities to obtain score between 0 and 1
+    } else if (type=="selectinrange") {
+      #strsplit the inputs to find min and max, give score 1 if the species is within the chosen range
+    }
+  }
+  for (crit in c("utilities", "form", "height","lifespan")){
+    #if (!grepl("any", inputsdata[[crit]])) {
+    #  database[,paste("score_", crit)]<-computecrit(database$crit, type=ifelse(crit=="utilities", ))
+    #}
+  }
+  
+  
+  
   #order the df by orderby, using latin name as id (ads an id variable, which is a factor with levels ordered by the orderby side)
   #icicicic I know it is not logical to do that here, it would be more logical to reorder the factor outside of the computation of the score
   # to do: separate computation of score and ordering of the species
@@ -523,7 +553,7 @@ compute_suitability_SCSM<-function(inputsdata=NULL,
   
   
   
-  #todo, compute effectiveness based on effect traits and inputdata of objectives
+  
   
   
   
