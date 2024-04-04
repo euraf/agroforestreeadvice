d6d8c028e4342e95bcfab33d8c2bd40c70ee07ff
kall12extra <tadeasstanek12@gmail.com>
2024-04-04 18:29:56 +0200

    For now, DFinfo calculated here. Later will try for other models - make it totaly general and implement in different place.
    Now the function is not hard-coded, it will load all "info" columns in data - if they incluced TRUE/FALSE - it will rename it by choice - so it can be later easilly translated. If the values are custom it will just use them. It will also agregate "info" columns of same "criteria" - eg, all food or wood columns are aggregated for each tree.

--- R/suitability_Czech.R
+++ R/suitability_Czech.R
@@ -47,8 +47,7 @@ compute_suitability_Czech<-function(inputsdata=NULL,
   print("fin suitability")
   
 
-  # print(inputsdata)
-  #write_xlsx(dbfinal, "00_dbfinal.xlsx") #for debugging
+
   #assign("dbfinal", dbfinal, envir = .GlobalEnv) # debugging, this is to be able to see the result in the console
   return(dbfinal)
   
@@ -92,3 +91,73 @@ Hard_criteria_filter <- function(db, inputsdata) {
   # write_xlsx(db, "01_dbfinal_filtered.xlsx") #for debugging
   return(db)
 }
+
+dfczechinfo <- function(interface, data) {
+    #loads all "info" side rows of interface, find their values for each tree in data and 
+    #then agregate them into one row if they share "criteria" value
+
+    #from interface - load rows where column side == "info"
+    datainfo2 <- data.frame(interface[interface$side == "info", c("criteria", "choice")])
+
+    #combine "choice" values of duplicated "criteria" values
+    datainfo2 <- aggregate(choice ~ criteria, data = datainfo2, paste, collapse = ", ")
+    datainfo3 <- data.frame(dataCzech["Scientific_name"])
+
+    #try each criteria from datainfo2$criteria as a column, if it exists, then assign the value from datainfo2 to the new column
+    for (i in 1:length(datainfo2$criteria)) {
+      if (datainfo2$criteria[i] %in% colnames(dataCzech)) {
+        datainfo3[datainfo2$criteria[i]] <- dataCzech[, datainfo2$criteria[i]]
+      }
+    }
+    
+    # loads all possible choices for each criteria
+    choices <- lapply(datainfo2$choice, function(x) strsplit(as.character(x), ", ")[[1]])
+
+    # iterate the choices, if the values is data column, append the column to datainfo3
+    for (i in 1:length(choices)) {
+      for (j in 1:length(choices[[i]])) {
+        if (choices[[i]][j] %in% colnames(dataCzech)) {
+          datainfo3[choices[[i]][j]] <- dataCzech[, choices[[i]][j]]
+        }
+      }
+    }
+
+    # turn list to vector
+    choices <- unlist(choices)
+    returned_text <- list()
+    for(i in seq_along(choices)) {  # finds answers in choice_en, choice_cz, etc. for each informative_criteria                                              
+      row <- interface[interface$choice == choices[i], ]
+      info <- row[, "choice"]
+      returned_text[[length(returned_text) + 1]] <- list(column = choices[i], info = info)
+    }
+
+    # try to match the column name with the column in datainfo3 and replace the value VRAI with returned_text
+    for(i in seq_along(returned_text)) {
+      if (returned_text[[i]]$column %in% colnames(datainfo3)) {
+        datainfo3[[returned_text[[i]]$column]][datainfo3[[returned_text[[i]]$column]] == "VRAI"] <- returned_text[[i]]$info
+      }
+    }
+
+    for (col in datainfo2$criteria) {
+      choice <- lapply(datainfo2$choice[datainfo2$criteria == col], function(x) strsplit(as.character(x), ", ")[[1]])
+      choice <- unlist(choice)
+      choice <- c(choice, col)
+      datainfo3[[col]] <- do.call(paste, c(lapply(choice, function(x) datainfo3[[x]]), sep = ", "))
+    }
+
+    #drop all columns other then (col in datainfo2$criteria)
+    datainfo3 <- datainfo3[, c("Scientific_name" ,datainfo2$criteria)]
+
+    # take whole datainfo3 and replace all FAUX with ""
+    datainfo4 <- data.frame(lapply(datainfo3, function(x) gsub("FAUX", "", x)))
+    datainfo4 <- data.frame(lapply(datainfo4, function(x) gsub(" , ", "", x)))
+
+    # Delete all "," at start of string
+    datainfo4 <- data.frame(lapply(datainfo4, function(x) gsub("^\\,+", "", x)))
+
+    # Delete all "," at the end of string
+    datainfo4 <- data.frame(lapply(datainfo4, function(x) gsub("\\,+$", "", x)))
+
+    return(datainfo4)
+}
+
