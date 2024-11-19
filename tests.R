2e985bc684f7303cfcf749feff882af205866c27
stanek-tadeas <tadeasstanek12@gmail.com>
2024-11-19 14:46:43 +0100

    Working list of actually used inputs

--- tests.R
+++ tests.R
@@ -1,96 +1,27 @@
-
-#library(plotly)
-library(shinydashboard) #for Dashboard appearance
-library(DT) #for Data Table
-library(bslib) #for tooltip
 library(dplyr)
-library(stringr)
-#library(tidyverse)
-library(purrr)          
-library(shiny.i18n)     # for translations in the app
-library(cowplot)        # for ggplot2 plots in download
-library(gridExtra)
-library(rsvg)           # convert svg to png in downloads
-##global----
-dataCzech<-read.table("models/dataCzech.txt", fileEncoding = "UTF-8", encoding = "UTF-8", fill=TRUE, sep=";", skipNul =TRUE, header=TRUE)
-interfaceCzech<-read.table("models/interfaceCzech.txt", fileEncoding = "UTF-8", encoding = "UTF-8",quote="", fill=TRUE, sep=";", header=TRUE)
-dataCzech <- data.frame(lapply(dataCzech, function(x) {if (is.character(x)) {return(trimws(x))} else {return(x)}}))
-interfaceCzech <- data.frame(lapply(interfaceCzech, function(x) {if (is.character(x)) {return(trimws(x))} else {return(x)}}))
 
-load("dfSuitability.RData")
-load("allinputs.RData")
+load("inputsdata.RData")
+load("interface.RData")
 
-Hard_criteria_filter <- function(db, inputsdata, interface) {
-  # Filter trees - if 999 in weightwithincriteria, then it is a hard criteria and algorithm will drop trees which do not meet it 
-  # it will check if values of these criteria is 1 - if not - drop them
-  # In esence, it checks what is hard criterium and then drops trees which do not have it as 1
-  db <- dfSuitability
-  inputsdata <- allinputs
-  interface <- interfaceCzech
+language <- "choice_cz"
+language <- gsub("choice", "", language)
 
-  
-  # Consolidate criteria from interface based on inputsdata and slider inputs
-  used_criteria <<- rbind(interface[interface$criteria %in% names(inputsdata) &
-                                    interface$weightwithincriteria == 999 & !is.na(interface$weightwithincriteria),],
-                         interface[interface$objecttype == "sliderInput",], interface[interface$objecttype == "checkboxGroupInput",])
+# convert named chr to data frame with two columns
+inputsdata2 <- data.frame(name = names(inputsdata), value = unname(inputsdata))
 
-  #drop info side
-  used_criteria <<- used_criteria[used_criteria$side != "info",]
+#in name column drop last char if it is number
+inputsdata2$name <- gsub("\\d$", "", inputsdata2$name)
 
-  # drop criteria which weightwithincriteria is not 999
-  used_criteria <- used_criteria[used_criteria$weightwithincriteria == 999,]
-  used_criteria_uniq <- unique(used_criteria$criteria)
+# find the $objecttype in interface where interface$criteria == inputsdata2$name
+inputsdata2$objecttype <- interface$objecttype[match(inputsdata2$name, interface$criteria)] 
 
-  unique_db <- db[FALSE, ]
-  unique_species <- unique(db$species)
+# find $name in interface where interface$criteria == inputsdata2$name and replace it by criteria_cz
+inputsdata2$name <- interface[[paste0("criteria", language)]][match(inputsdata2$name, interface$criteria)]
 
-  # loop species, find values for each relevant criteria of the species, and if there is a zero, then add "yes" to delete column
-  for (species in unique_species) {
-      species_data <- db[db$species == species, ]
-      
-      # Get only the criteria that are relevant
-      criteria_data <- species_data[species_data$criteria %in% used_criteria_uniq, ]
-      
-      # Get unique values of the criteria
-      unique_values <- unique(criteria_data$value)
-      unique_criteria <- unique(criteria_data$criteria)
-      
-      # Determine if there are any zeros in the values
-      delete_flag <- if("0" %in% unique_values) "yes" else "no"
-      
-      # Create a new row to be added to unique_db
-      new_row <- data.frame(
-          species = species,
-          value = paste(unique_values, collapse = ", "),
-          criteria = paste(unique_criteria, collapse = ", "),
-          delete = delete_flag)
-      
-      # Append new row to unique_db
-      unique_db <- rbind(unique_db, new_row)
-  }
+# do the same for inputsdata2$value and interface$choice
+inputsdata2$value <- ifelse(
+  is.na(match(inputsdata2$value, interface$choice)),
+  inputsdata2$value,
+  interface[[paste0("choice", language)]][match(inputsdata2$value, interface$choice)]
+)
 
-  # Select species where 'delete' is "yes"
-  delete_species <- unique_db$species[unique_db$delete == "yes"]
-
-  # drop these species
-  db <- db[!db$species %in% delete_species, ]
-
-  return(db)
-}
-xxx3 <-Hard_criteria_filter()
-
-
-
-
-
-
-
-# Filter the DataFrame where 'weightwithincriteria' is 999
-df <- interfaceCzech
-filtered_df = df[df['weightwithincriteria'] == 999]
-
-# Extract the 'criteria' column
-criteria_data = filtered_df['criteria']
-
-# Print the extracted data
-print(criteria_data)
